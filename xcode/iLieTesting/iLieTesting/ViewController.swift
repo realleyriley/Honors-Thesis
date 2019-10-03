0bc3cb3aa426c7330a555f70ddf91b5c299942e9
Riley Tallman <rptallman@gmail.com>
2019-10-03 13:37:50 -0700

    more updates

--- xcode/iLieTesting/iLieTesting/ViewController.swift
+++ xcode/iLieTesting/iLieTesting/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController, WKNavigationDelegate, AVCaptureVideoData
         webView.load(request)
         
         let videoCaptureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)  // loads the front camera with default settings on startup
-        do{
+        do {
             let videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice!)    // stream of input?
             captureSession = AVCaptureSession()
             captureSession?.addInput(videoInput)
@@ -65,16 +65,21 @@ class ViewController: UIViewController, WKNavigationDelegate, AVCaptureVideoData
     
     // portrait = 1, upsidedown = 2, landscapeLeft = 3, landscapeRight = 4
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        print("-------- View Will Transition --------")
+//        print("-------- View Will Transition --------")
         let gravity_orientation = UIDevice.current.orientation.rawValue     // this is the orientation that the device *wants* to rotate to
-        print("Current orientation: ", gravity_orientation)
+//        print("Current orientation: ", gravity_orientation)
         
+//        let startTime = DispatchTime.now() // <<<<<<<<<< Start time
+
         // here is where the magic happens
         let coreML_output = VNCoreMLRequest(model: self.model) { (finishedRequest, error) in
             // grab the inference results
             guard (finishedRequest.results as? [VNClassificationObservation]) != nil else { return }
         }
         
+        
+        let startTime = DispatchTime.now() // <<<<<<<<<< Start timing how long it takes to analyze images with our model
+
         // get the scores for two images. We want to have more confidence in our decision, so we can check multiple images
         try? VNImageRequestHandler(cvPixelBuffer: CVpixel!, options: [:]).perform([coreML_output])
         var observation_zero: VNClassificationObservation = coreML_output.results!.first as! VNClassificationObservation
@@ -85,7 +90,8 @@ class ViewController: UIViewController, WKNavigationDelegate, AVCaptureVideoData
         var override_gravity: Bool = false
         
         // loop over 8 more images at max if we don't get confident scores. If we get a good enough confidence for two consecutive images at a time, then we can break
-        while(i < 10) {
+        let n = 8
+        while(i < n) {
             // the two outputs need to be the same
             if (observation_zero.identifier == observation_one.identifier){
                 if (observation_zero.confidence > 0.75 && observation_one.confidence > 0.75){
@@ -94,7 +100,7 @@ class ViewController: UIViewController, WKNavigationDelegate, AVCaptureVideoData
                 }
             }
             
-            print("low confidence.. getting more images")
+//            print("low confidence.. getting more images")
             // ! means to abort the execution if that variable is nil
             try? VNImageRequestHandler(cvPixelBuffer: CVpixel!, options: [:]).perform([coreML_output])
             
@@ -106,6 +112,17 @@ class ViewController: UIViewController, WKNavigationDelegate, AVCaptureVideoData
             
             i = i + 1
         }
+        
+        let endTime = DispatchTime.now()   // <<<<<<<<<<   end time
+        let milliseconds = Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000 // <<<<< Difference in milli seconds (UInt64)
+        
+        // use these 3 lines to collect data on the ammount of time taken for analyzing n images
+        if(i == n) {
+            print(milliseconds)
+        }
+        
+        print("Took \(milliseconds) ms to anlyze \(i) images")
+
         
         // set the label text
         var predictedOrientation: String?
@@ -123,11 +140,11 @@ class ViewController: UIViewController, WKNavigationDelegate, AVCaptureVideoData
         case .some(_):
             predictedOrientation = ".some(_)"
         }
+        
 //        self.predictedOrientation.text = predictedOrientation
         print("predOrient: ", predictedOrientation!)
         
         let predconfidence = String(format: "%.01f%%%", observation_one.confidence * 100)
-//        self.confidence.text = "\(predconfidence)"
         print("predConf: ", predconfidence)
         
         
@@ -139,13 +156,11 @@ class ViewController: UIViewController, WKNavigationDelegate, AVCaptureVideoData
             }
             else {
                 // since they don't agree, override the gravity rotation
-                print("OVERRIDE")
+//                print("OVERRIDE")
                 UIDevice.current.setValue(Int(observation_one.identifier), forKey: "orientation")
             }
         }
         else {
-            predictedOrientation = predictedOrientation! + " - Grav"
-//            self.predictedOrientation.text = predictedOrientation
             print("fall back on gravity")
         }
     }
