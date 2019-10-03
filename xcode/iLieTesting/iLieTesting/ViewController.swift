3014087958ed6c58bfd08ea44ab910133f06ae3f
Riley Tallman <rptallman@gmail.com>
2019-10-02 22:00:39 -0700

    working webview :)

---
+++ xcode/iLieTesting/iLieTesting/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iLieTesting
+//
+//  Created by Communist Hacker on 10/2/19.
+//  Copyright © 2019 Tallman. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.eia.gov/tools/faqs/faq.php?id=427&t=3")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+    }
+
+}
+
